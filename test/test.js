ccbd8c0846d0d09f2c133d895b25276ea7618f63
jupe <jussiva@gmail.com>
2013-06-04 07:26:06 +0300

    Update test.js
    update tests. This is more like example and will be moved to separate folder in future.

--- test/test.js
+++ test/test.js
@@ -4,28 +4,65 @@ var mti = require('../');
 
 mongoose.connect('mongodb://localhost/mti');
 
-mti.init('test', {minute: true, second: true, millisecond: true}); //second: Boolean, minute: Boolean
+mti.init('test', {interval: 1}); //second: Boolean, minute: Boolean
+
+
+var i=0;
+
 
 mti.push(new Date(), i, false, {}, function(err, docs){});
 var timer;
-var i=0;
+
 setTimeout( function(){
   
   timer = setInterval( function(){
     mti.push(new Date(), i+=1, false, {}, function(err, docs){
-      if(err) console.log(err);
-      if(docs) console.log('saved');
+      if(err) {
+        console.log(err);
+      }
+      //if(docs) console.log('saved');
     });  
   }, 1000);
   
   setTimeout( function(){
+    console.log('stop looping');
     timer.stop();
-  },10000);
-
+  },5000);
 },1000);
 
 
 
+//var format = 'hash'
+var format = '[x,y]'
+//var format = '[ms,y]'
+mti.findData( {
+    from: new Date(new Date()-1000*60*60*24*2), to: new Date(), 
+    condition: {}, 
+    interval: 60, 
+    format: format}, 
+    function(error, data){
+  if(error)console.log(error);
+  else console.log('len: '+data.length);
+});
+
+mti.findMin( {
+    from: new Date(new Date()-1000*60*60*24), to: new Date(), 
+    condition: {}, 
+    interval: 60, 
+    format: format}, 
+    function(error, data){
+  if(error)console.log(error);
+  else console.log('min: '+data);
+});
+mti.findMax( {
+    from: new Date(new Date()-1000*60*60*24), to: new Date(), 
+    condition: {}, 
+    interval: 60, 
+    format: format}, 
+    function(error, data){
+  if(error)console.log(error);
+  else console.log('max: '+data);
+});
 /*
 describe('mongoose-ts', function() {
   it('init', function() {
